76ac4315f469d1274aba40224ccc1688042e6c82
prudhvipotnuru <prudhvipotnuru@gmail.com>
2026-02-27 15:51:32 +0530

    added swagger documentation for REST APIs

--- src/main/java/com/prudhvi/xmlJson/XmlJsonApplication.java
+++ src/main/java/com/prudhvi/xmlJson/XmlJsonApplication.java
@@ -3,6 +3,16 @@ package com.prudhvi.xmlJson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Ticket API",
+				version = "1.0",
+				description = "API documentation for booking ticket"
+		)
+)
 @SpringBootApplication
 public class XmlJsonApplication {
 
